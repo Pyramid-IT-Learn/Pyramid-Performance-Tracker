698bb1a15cf1e0bec693dd87642293fa74365e79
dog-broad <rushyendra.7004@gmail.com>
2024-09-09 22:14:25 +0530

    Refactor file deletion logic to handle permission errors

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import errno
 import os
 import argparse
 import shutil
@@ -91,7 +92,16 @@ def main():
                     file_path = os.path.join(folder, filename)
                     try:
                         if os.path.isfile(file_path) or os.path.islink(file_path):
-                            os.unlink(file_path)
+                            try:
+                                os.unlink(file_path)
+                            except OSError as e:
+                                print('Failed to delete %s. Reason: %s' % (file_path, e))
+                                print('Trying to clear the contents of the directory...')
+                                if e.errno == errno.EACCES:
+                                    with open(file_path, 'w'):
+                                        pass
+                                else:
+                                    raise
                         elif os.path.isdir(file_path):
                             shutil.rmtree(file_path)
                     except Exception as e:
